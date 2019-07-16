6bdfc63effb245496b269fe8cf3643c646f87f5b
Annieli Siegel <duddoodler@gmail.com>
2019-07-16 13:48:40 +0300

    delete Butt Update

--- VirusList_adMyPage.php
+++ VirusList_adMyPage.php
@@ -1,18 +1,24 @@
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Virus Name</th>
+                        <th scope="col">Virus Description</th>
+                    </tr>
+                </thead>
+                <tbody id="vadTable">
+                </tbody>
+            </table>
+        </div>
+    </div>
 
-    <div class="col-9">
-        <table class="table">
-            <thead class="thead-dark">
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Virus Name</th>
-                    <th scope="col">Virus Description</th>
-                </tr>
-            </thead>
-            <tbody id="vadTable">
-            </tbody>
-        </table>
+
+    <div class="row">
+        <div class="col">
+            <a id="delUserButt">Delete User</a>
+        </div>
+    </div>
 </div>
-
-
-<a id="delUserButt">Delete User</a>
-
--- adMyPage.php
+++ adMyPage.php
@@ -51,7 +51,7 @@ include 'URLset.php';
                     </div>
                     <div class="col-9">
                         <div class="tab-content" id="v-pills-tabContent">
-                            <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab"><?php include 'VirusList_adMyPage.php' ?></div>
+                            <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab"><?php include 'VirusList_adMyPage.php' ?> </div>
                             <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
                             <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab"><?php include 'adMess.php' ?></div>
                             <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
